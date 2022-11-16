591ab668f925ea8eba9de47c60e5b1deda1b0751
Joel Bartosch <50476983+TheKineticz@users.noreply.github.com>
2022-11-16 17:16:01 +1100

    Basic functionality of ex1

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+int StrToInt(string strNumber){
+    int number = 0;
+    foreach (char c in strNumber){
+        number *= 10;
+        number += c - '0';
+    }
+    return number;
+}
+
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/strtoint", (string number) => {
+    return StrToInt(number);
+});
 
 app.Run();
