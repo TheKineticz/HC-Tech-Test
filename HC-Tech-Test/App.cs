2c2dab23d15eb9ccc278cd37ff92597e40ca4fad
Joel Bartosch <50476983+TheKineticz@users.noreply.github.com>
2022-11-17 20:23:49 +1100

    Refactor tests to another project

--- HC-Tech-Test/App.cs
+++ HC-Tech-Test/App.cs
@@ -1,7 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 app.MapGet("/exa", ExA.Endpoint);
 app.MapGet("/exb", ExB.Endpoint);
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
